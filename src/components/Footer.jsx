5e87fb1bee014fc9e3b9815347ba246cd0503366
osamaxqureshi <osamaxqureshi@yahoo.com>
2023-10-13 20:16:00 +0500

    initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const currentYear = new Date().getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright &copy; {currentYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+
+const Note = () => {
+    return (
+        <div className="note">
+            <h1>Note</h1>
+            <p>This is the note component</p>
+        </div>
+    );
+}
+
+export default Note;
