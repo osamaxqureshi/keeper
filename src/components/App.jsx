26ad2d440051d4db4ef4e7ed7ab42453547de4d2
Osama Ahmed Qureshi <osamaxqureshi@outlook.com>
2023-12-03 17:36:07 +0500

    updated keeper app to include additional notes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
-// import React from'react';
+import React from'react';
 import Note from './Note';
 import Header from './Header';
 import Footer from './Footer';
+import notes from "../notes";
+
 
 const App = () => {
     return (
         <div>
-            <Header />
-            <Note />
-            <Footer />
+ <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
         </div>
     );
 }
